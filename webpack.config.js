9f822eb3e219a3ace1ce5d0700e8bfa779d14a57
Yahir Cardona <yahir.cardona@gmail.com>
2021-02-05 11:55:51 -0600

    Webpack Configurated

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,27 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index',
-
+  entry: './src/index.js',
   output: {
-    filename: 'main.bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '',
   },
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: { minimize: true },
-          },
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
+  module:{
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    },
+    {
+        test: /\.html$/i,
+        loader: 'html-loader',
       },
       {
         test: /\.css$/,
@@ -49,25 +39,24 @@ module.exports = {
           },
         ],
       },
-      /*
-      {
-        test: /\.scss$/,
+    {
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
-        ]
-      } */
-    ],
-  },
+        {
+            loader: 'file-loader',
+        },
+    ]
+    }
+  ],
+},
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
+        template: './src/index.html',
+        filename: './index.html',
+      }),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-  ],
-};
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      }),
+  ]
+}
