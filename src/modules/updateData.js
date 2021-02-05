cb7b395d996782ba047b3d96a821a41c2c7b69ab
Yahir Cardona <yahir.cardona@gmail.com>
2021-02-05 16:10:43 -0600

    linter errors fixed

--- src/modules/updateData.js
+++ src/modules/updateData.js
@@ -3,30 +3,32 @@ const input = document.querySelector('[data-city-search]');
 // const main = document.querySelector('#name');
 // const temp = document.querySelector('[data-temperature]');
 // const desc = document.querySelector('.desc');
-const locationElement = document.querySelector('[data-location]')
-const statusElement = document.querySelector('[data-status]')
-const temperatureElement = document.querySelector('[data-temperature]')
-const rainElement = document.querySelector('[data-precipitation]')
-const windElement = document.querySelector('[data-wind]')
+const locationElement = document.querySelector('[data-location]');
+const statusElement = document.querySelector('[data-status]');
+const temperatureElement = document.querySelector('[data-temperature]');
+const rainElement = document.querySelector('[data-precipitation]');
+const windElement = document.querySelector('[data-wind]');
 
 const apiKey = '0e2959b47fb2ec863a63504b4e25870a';
 
-input.addEventListener('click', () => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}`;
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const tempValue = data.main.temp;
-      const nameValue = data.name;
-      const descValue = data.weather[0].description;
-      const tempCelcius = parseInt(tempValue - 273.15, 10);
-      const windValue = data['wind']['speed'];
-      const rainValue = data['clouds']['all'];
-      locationElement.textContent = nameValue;
-      statusElement.textContent = descValue;
-      temperatureElement.textContent = `Temp - ${tempCelcius} C`;
-      windElement.textContent = windValue + 'm/s';
-      rainElement.textContent = rainValue + '%';
-    });
-  input.value = '';
+input.addEventListener('keypress', e => {
+  if (e.key === 'Enter') {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const tempValue = data.main.temp;
+        const nameValue = data.name;
+        const descValue = data.weather[0].description;
+        const tempCelcius = parseInt(tempValue - 273.15, 10);
+        const windValue = data.wind.speed;
+        const rainValue = data.clouds.all;
+        locationElement.textContent = nameValue;
+        statusElement.textContent = descValue;
+        temperatureElement.textContent = `Temp - ${tempCelcius} C`;
+        windElement.textContent = `${windValue}m/s`;
+        rainElement.textContent = `${rainValue}%`;
+      });
+    input.value = '';
+  }
 });
