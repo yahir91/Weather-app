6bca17be7640daf2d944a5ce5958702313e3b9d1
Yahir Cardona <yahir.cardona@gmail.com>
2021-02-05 15:59:26 -0600

    style improved

--- src/modules/updateData.js
+++ src/modules/updateData.js
@@ -1,12 +1,17 @@
-const input = document.querySelector('.input_text');
-const button = document.querySelector('.submit');
-const main = document.querySelector('#name');
-const temp = document.querySelector('.temp');
-const desc = document.querySelector('.desc');
+const input = document.querySelector('[data-city-search]');
+// const button = document.querySelector('.submit');
+// const main = document.querySelector('#name');
+// const temp = document.querySelector('[data-temperature]');
+// const desc = document.querySelector('.desc');
+const locationElement = document.querySelector('[data-location]')
+const statusElement = document.querySelector('[data-status]')
+const temperatureElement = document.querySelector('[data-temperature]')
+// const precipitationElement = document.querySelector('[data-precipitation]')
+const windElement = document.querySelector('[data-wind]')
 
 const apiKey = '0e2959b47fb2ec863a63504b4e25870a';
 
-button.addEventListener('click', () => {
+input.addEventListener('click', () => {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}`;
   fetch(url)
     .then(response => response.json())
@@ -15,9 +20,11 @@ button.addEventListener('click', () => {
       const nameValue = data.name;
       const descValue = data.weather[0].description;
       const tempCelcius = parseInt(tempValue - 273.15, 10);
-      main.innerHTML = nameValue;
-      desc.innerHTML = descValue;
-      temp.innerHTML = `Temp - ${tempCelcius}\`C`;
+      const windValue = data.wind.windspeed;
+      locationElement.textContent = nameValue;
+      statusElement.textContent = descValue;
+      temperatureElement.textContent = `Temp - ${tempCelcius} C`;
+      windElement.textContent = windValue;
     });
   input.value = '';
 });
