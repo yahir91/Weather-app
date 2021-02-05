8a21bf7c06357777962da975858bb0f44a7786d8
Yahir Cardona <yahir.cardona@gmail.com>
2021-02-05 15:10:19 -0600

    Html and Css template created

---
+++ src/modules/updateData.js
@@ -0,0 +1,29 @@
+import putData from './fetchData.js'
+
+let input = document.querySelector('.input_text');
+let button= document.querySelector('.submit');
+let main = document.querySelector('#name');
+let temp = document.querySelector('.temp');
+let desc = document.querySelector('.desc');
+
+const apiKey = "0e2959b47fb2ec863a63504b4e25870a"
+
+button.addEventListener('click', () => {
+    console.log(4)
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}`
+    console.log(url)
+    fetch(url)
+    .then(response => response.json())
+    .then(data =>{ 
+        let tempValue = data['main']['temp'];
+        let nameValue = data['name'];
+        let descValue = data['weather'][0]['description'];
+        console.log(tempValue)
+        let tempCelcius = parseInt(tempValue -273.15, 10) 
+        main.innerHTML = nameValue;
+        desc.innerHTML = descValue;
+        temp.innerHTML = "Temp - "+tempCelcius+"C";
+    }).catch(err => alert("Wrong city name!"));
+    console.log(obj.main)
+    input.value = ""
+    })
